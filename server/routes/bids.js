9584d14eee7a033794d1600e7f26cab9baf60b6b
qiuxin <qiuxin@qiuxindeMacBook-Air.local>
2018-12-02 13:34:03 -0500

    1. move more logic from router to controller, make router cleaner, 2. extract some logic of determine whether should add a new bid to a separate function and add unit test to it 3. refactor bids controller a bit for better understanding, 4. update package.json as per updates

--- server/routes/bids.js
+++ server/routes/bids.js
@@ -7,33 +7,29 @@ var bids_validation = require('./validations/bids');
 
 /* get the winning bid of a car */
 router.route('/:car_id/winner').get(validate(bids_validation.getWinningBidByCarId), function(req, res) {
-    var car_id = req.params.car_id;
 
-    bidsController.getWinningBidByCarId(car_id, function (data) {
-        res.json(helper.createResult(true, { car_id: car_id, winner: data }));
+    bidsController.getWinningBidByCarId(req, function (error, data) {
+        
+        res.json(helper.createResult(!error, error? error:data))
     });
 
 });
 
 /* get all bids history of a car */
 router.route('/:car_id').get(validate(bids_validation.getAllBidsByCarId), function(req, res) {
-    var car_id = req.params.car_id;
 
-    bidsController.getAllBidsByCarId(car_id, function (data) {
-        res.json(helper.createResult(true, { car_id: car_id, bids: data }));
+    bidsController.getAllBidsByCarId(req, function (error, data) {
+        res.json(helper.createResult(!error, error? error:data))
+        
     });
 
 });
 
 /* post new bid on a given car_id */
 router.route('/:car_id').post(validate(bids_validation.addBidByCarId), function (req, res) {
-    var bid_data = req.body;
-    var car_id = req.params.car_id;
 
-    bid_data = Object.assign(bid_data, {car_id: car_id});
-    bidsController.addBid(bid_data, function (success) {
-        var r =  helper.createResult(success, success? { message: 'success' } : { message:'Bad Request: user and car must be present' } );
-        res.json(r);
+    bidsController.addBid(req, function (error, data) {
+        res.json(helper.createResult(!error, error? error:data))
     });
 });
 
---
+++ test/bidsController.test.js
@@ -0,0 +1,40 @@
+var expect = require('chai').expect;
+var should = require('chai').should();
+var rewire = require('rewire');
+var controller = rewire('../server/controllers/bidsController');
+var shouldAddNewBidByBiddingValue = controller.__get__('shouldAddNewBidByBiddingValue'); 
+
+describe('test bidsController::shouldAddNewBidByBiddingValue', function() {
+
+    it('it should return false when when bidding_value is lower than current bids', function(done) {
+        var current_bids = [{
+            user_id: 1,
+            bidding_value: 12.00
+        }];
+        
+        var result = shouldAddNewBidByBiddingValue(12.00, current_bids);
+        expect(result).to.be.a('boolean').equal(false);
+        result = shouldAddNewBidByBiddingValue(11.00, current_bids);
+        expect(result).to.be.a('boolean').equal(false);
+        done();
+    });
+
+    it('it should return true when when bidding_value is greater than current bids', function(done) {
+        var current_bids = [{
+            user_id: 1,
+            bidding_value: 12.00
+        }];
+        var result = shouldAddNewBidByBiddingValue(123.00, current_bids);
+        expect(result).to.be.a('boolean').equal(true);
+        done();
+    });
+
+    it('it should return true when when current_bids is empty', function(done) {
+
+        var current_bids = [];
+        var result = shouldAddNewBidByBiddingValue(1.00, current_bids);
+        expect(result).to.be.a('boolean').equal(true);
+        done();
+    });
+
+});
